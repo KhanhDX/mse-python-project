7db181bfa052f4cdf9893eac0ecc651a760fa80e
ngoctu1212 <39819572+ngoctu1212@users.noreply.github.com>
2022-10-09 18:48:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -86,5 +86,31 @@ def exportDataToFile():
     flash("Export file successfully!")
     return redirect(url_for('index'))
 
+def crawlData():
+
+    lst = []
+    url = "http://127.0.0.1:5000/"
+    html = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup.find_all('td')
+    for i in range(0, len(tags), 8):
+        data = ''
+        data = data + 'RollNumber:' + tags[i].text + '\n'
+        data = data + 'First Name:' + tags[i].text + '\n'
+        data = data + 'Last Name:' + tags[i].text + '\n'
+        data = data + 'Email:' + tags[i].text + '\n'
+        data = data + 'Date of birth:' + tags[i].text + '\n'
+        data = data + 'Address:' + tags[i].text + '\n'
+        data = data + 'Score:' + tags[i].text + '\n'
+        lst.append(data)
+    with open('dataCrawl.txt', 'w') as file:
+        for i in range(0, len(lst)):
+            s = 'Thong tin sinh vien thu: '
+            s = s + str(i + 1) + '\n'
+            file.write(s)
+            file.write(lst[i])
+            file.write("\n")
+
 if __name__ == '__main__':
     app.run(debug=True)
+    crawlData()
