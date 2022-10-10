8a1a664f6a23117b4daa294ca1d10128cc959761
ngoctu1212 <39819572+ngoctu1212@users.noreply.github.com>
2022-10-10 09:21:25 +0700

    Update data_collector.py

--- data_collector.py
+++ data_collector.py
@@ -10,12 +10,12 @@ def crawlData():
     for i in range(0, len(tags), 8):
         data = ''
         data = data + 'RollNumber:' + tags[i].text + '\n'
-        data = data + 'First Name:' + tags[i].text + '\n'
-        data = data + 'Last Name:' + tags[i].text + '\n'
-        data = data + 'Email:' + tags[i].text + '\n'
-        data = data + 'Date of birth:' + tags[i].text + '\n'
-        data = data + 'Address:' + tags[i].text + '\n'
-        data = data + 'Score:' + tags[i].text + '\n'
+        data = data + 'First Name:' + tags[i+1].text + '\n'
+        data = data + 'Last Name:' + tags[i+2].text + '\n'
+        data = data + 'Email:' + tags[i+3].text + '\n'
+        data = data + 'Date of birth:' + tags[i+4].text + '\n'
+        data = data + 'Address:' + tags[i+5].text + '\n'
+        data = data + 'Score:' + tags[i+6].text + '\n'
         lst.append(data)
     with open('dataCrawl.txt', 'w', encoding='utf-8') as file:
         for i in range(0, len(lst)):
